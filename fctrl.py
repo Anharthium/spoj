4f4313b9d4e7610ecb2f568d947df752a4cbdfd7
Aravindh Sathiyamoorthy <arvind.sathiyamoorthy@gmail.com>
2019-08-30 16:28:27 +0530

    fctrl.py accepted solution

--- fctrl.py
+++ fctrl.py
@@ -1,80 +1,49 @@
 from math import factorial, log
 
-#def calc_trail_0_br(x):
-#    """
-#    Brute force method to calculate trailing zeroes in factorial of x.
-#    """
-#    fact = str(factorial(x))
-#    fact = fact[::-1]
-#    trail_zero = 0
-#    for dig in fact:
-#        if dig == '0':
-#            trail_zero += 1
-#        else:
-#            return trail_zero
-
-# look up table for number of fives
-# in prime factorization of any multiple of 5.
-num_five_prfct = {5:1, 10:1}
-
-
-def five_in_prfct(x):
-    """
-    function to calculate number of 5's in prime factorization of x.
-    """
-    num_five = 0
-    orig_num = x
-
-    # non multiples of 5 can't produce factor '5'.
-    while (True):
-        if (x%5 != 0):
-            break
-        else:
-            if x in num_five_prfct:
-
-            num_five += 1
-            x = x//5
-    
-    # store in the look up table
-    num_five_prfct[orig_num] = num_five
-
-    return num_five
-
-def calc_trail_0_sm(x):
-    """
-    A smart way to calculate trailing zeroes in factorial of x.
-    """
-    start_num = x - (x%5)   # nearest multiple of 5 <= x
-
+"""
+def calc_trail_0_br(x):
+    fact = str(factorial(x))
+    fact = fact[::-1]
     trail_zero = 0
-
-    for i in range(start_num, 0, -5):
-        if i in num_five_prfct:
-            trail_zero += num_five_prfct[i]
+    for dig in fact:
+        if dig == '0':
+            trail_zero += 1
         else:
-            trail_zero += five_in_prfct(i)
+            return trail_zero
+"""
+
+def calc_trail_0_sm1(x):
+    """
+    A smart way to calculate trailing zeroes in factorial of x [version1].
+
+    number of trailing zeroes = 5's contributed by multiples of 5 in the factorial
+    i.e x/5 + x/25 + x/125 + x/625 + ...
+    """
+    
+    trail_zero = 0
+    i = 5
+    while (True):
+        contri_5 = x//i
+        if (contri_5 == 0):
+            break
+        trail_zero += contri_5
+        i = i*5
 
     return trail_zero
 
-
-#def main():
-#    """
-#    Driver function.
-#    """
-#    
-#    for i in range(1, 10000):
-#        br = calc_trail_0_br(i)
-#        sm = calc_trail_0_sm(i)
-#        if (br != sm):
-#            print("num = {}, trail_0_br = {}, trail_0_sm = {}".format(i, br, sm))
-
+"""
+def main():
+    for i in range(1, 10000):
+        br = calc_trail_0_br(i)
+        sm = calc_trail_0_sm1(i)
+        print("num = {}, trail_0_br = {}, trail_0_sm = {}".format(i, br, sm))
+"""
 
 def main():
     t_case = eval(input())
     for i in range(t_case):
         i_num = eval(input())
-        print(calc_trail_0_sm(i_num))
-
+        print(calc_trail_0_sm1(i_num))
 
 main() # call main
 
