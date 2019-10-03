11deceab960c3066b25855173e18d2c87f938126
Aravindh Sathiyamoorthy <arvind.sathiyamoorthy@gmail.com>
2019-10-03 23:03:41 +0530

    modified onp.cpp - still not accepted

--- onp.cpp
+++ onp.cpp
@@ -30,10 +30,12 @@ short op_precedence(char op) {
 }
 
 inline bool is_operator(char sym) {
+    // is sym an operator?
     return (sym == '+' || sym == '-' || sym == '*' || sym == '/' || sym == '^' || sym == '(');
 }
 
 inline bool is_operand(char sym) {
+    // is sym an operand? 
     return (sym >= 'a' && sym <= 'z');
 }
 
@@ -46,12 +48,12 @@ void in_to_post(string & expr) {
         if (is_operator(expr[i])) { // operator
             // pop op_stack until the 
             // top of the stack has less precedence 
-            // than curr operator
-            if (op_st.empty()) { // stack is empty; straight away push
-                op_st.push(std::make_pair(expr[i], op_precedence(expr[i])));
-                continue;
-            }
+            // than curr operator or stack is empty
             while(1) {
+                if (op_st.empty()) { // stack is empty; straight away push
+                    op_st.push(std::make_pair(expr[i], op_precedence(expr[i])));
+                    break;
+                }
                 pair <char, short> & top_op = op_st.top();
                 if (op_precedence(top_op.second) >= op_precedence(expr[i])) {
                     cout << top_op.first;
@@ -78,8 +80,8 @@ void in_to_post(string & expr) {
                     break;
                 }
                 else {
-                    op_st.pop();
                     cout << top_op.first;
+                    op_st.pop();
                 }
             }
         }
@@ -99,7 +101,9 @@ int main() {
     for (int i = 0; i < t; ++i) {
         string expr;
         cin >> expr;
+        //cout << expr.length() << endl;
         in_to_post(expr);
         cout << "\n";
     }
+    return 0;
 }
